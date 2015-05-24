60d3877e664a5a35052b4e4f8b63ef0766957a01
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-05-24 19:35:19 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lngTesterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lngTesterApp
+ */
+angular.module('lngTesterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
