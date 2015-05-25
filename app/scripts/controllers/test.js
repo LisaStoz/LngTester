eb6815bf831b4d6a83b8500f824b97ab4d7fecf1
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-05-25 18:53:38 +0100

    correct and incorrect answer - show result

--- app/scripts/controllers/test.js
+++ app/scripts/controllers/test.js
@@ -10,9 +10,30 @@
 angular.module('lngTesterApp')
   .controller('TestCtrl', function ($scope, questions) {
 
+    var correct;
+
     $scope.question = questions.next();
 
     $scope.submitAnswer = function(){
-      questions.answer($scope.user && ($scope.user.answer === $scope.question.answer));
+      correct = $scope.user !== undefined && $scope.user.answer === $scope.question.answer;
+      questions.answer(correct);
     };
+
+    $scope.isCorrect = function(){
+      return correct === true;
+    };
+
+    $scope.isIncorrect = function(){
+      return correct === false;
+    };
+
+    $scope.notSubmitted = function() {
+      return !$scope.isCorrect() && !$scope.isIncorrect();
+    };
+
+    $scope.nextQuestion = function(){
+      correct = undefined;
+      $scope.question = questions.next();
+    };
+
   });
