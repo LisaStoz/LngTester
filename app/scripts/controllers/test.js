57ab41b31f5bd7048ffb3aedbf5d62c2b4b6e576
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-05-25 18:35:49 +0100

    allow a user to submit an answer

--- app/scripts/controllers/test.js
+++ app/scripts/controllers/test.js
@@ -9,5 +9,10 @@
  */
 angular.module('lngTesterApp')
   .controller('TestCtrl', function ($scope, questions) {
+
     $scope.question = questions.next();
+
+    $scope.submitAnswer = function(){
+      questions.answer($scope.user && ($scope.user.answer === $scope.question.answer));
+    };
   });
